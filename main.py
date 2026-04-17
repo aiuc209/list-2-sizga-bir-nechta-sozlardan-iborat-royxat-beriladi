0ee146d19045bf8bf70dfdce9c2019a31c40d988
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:26:15 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+List 2 Sizga Bir Nechta Sozlardan Iborat Royxat Beriladi
+Topic: LIST – 2 ��Sizga bir nechta so‘zlardan iborat ro‘yxat beriladi.
+"""
+
+def main():
+    print("Welcome to List 2 Sizga Bir Nechta Sozlardan Iborat Royxat Beriladi!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
